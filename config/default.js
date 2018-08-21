3fe5c5939f27fe6609d274116a1b0b2425ec1a9b
gzeller <gabriel.d.celery@gmail.com>
2018-08-21 18:17:22 +0100

    - Changed config

--- config/default.js
+++ config/default.js
@@ -3,7 +3,6 @@
 module.exports = {
     host: require('./host'),
     alxAPI: require('./alxAPI'),
-    auth: require('./auth'),
     targetGroups: require('./targetGroups'),
     auth0: require('./auth0'),
     files: require('./files')
